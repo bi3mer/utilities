6c1844be90eb302caf8ab59bc020239f5c6f96cc
bi3mer <colan@comcast.net>
2026-03-18 11:06:52 -0400

    beep on mac working for utility-timer

--- utility-timer/bin/main.ml
+++ utility-timer/bin/main.ml
@@ -2,6 +2,13 @@ let parse_args () =
   if Array.length Sys.argv < 2 then None
   else int_of_string_opt Sys.argv.(1)
 
+
+let beep () =
+  if Sys.file_exists "/usr/bin/pw-play" then
+    ignore (Sys.command "pw-play /usr/share/sounds/freedesktop/stereo/complete.oga")
+  else
+    ignore (Sys.command "afplay /System/Library/Sounds/Glass.aiff")
+
 let () =
   match parse_args () with
   | None ->
@@ -13,6 +20,5 @@ let () =
       Unix.sleepf 1.0
     done;
 
-
     Printf.printf "\rDONE!\n%!";
-    ignore (Sys.command "pw-play /usr/share/sounds/freedesktop/stereo/complete.oga")
+    beep ()
