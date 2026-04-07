f4b05b5f41ad22d986562fccbe461cc14a4a8c22
Colan Biemer <bi3mer93@gmail.com>
2026-04-07 09:12:43 -0400

    timer now has formatted time and the user can input seconds or minutes

--- utility-timer/bin/main.ml
+++ utility-timer/bin/main.ml
@@ -1,5 +1,17 @@
+type duration = Seconds of int | Minutes of int
+
 let parse_args () =
-  if Array.length Sys.argv < 2 then None else int_of_string_opt Sys.argv.(1)
+  let argv = Sys.argv in
+  let argc = Array.length argv in
+  if argc < 3 then None
+  else
+    match (argv.(1), int_of_string_opt argv.(2)) with
+    | "-s", Some n -> Some (Seconds n)
+    | "-m", Some n -> Some (Minutes n)
+    | _ -> None
+
+let format_time seconds =
+  Printf.sprintf "%d:%02d" (seconds / 60) (seconds mod 60)
 
 let beep () =
   if Sys.file_exists "/usr/bin/pw-play" then
@@ -10,11 +22,18 @@ let beep () =
 let () =
   match parse_args () with
   | None ->
-      print_endline "Usage: utility-timer <seconds : integer>";
+      Printf.eprintf "Usage: %s -s <seconds> OR -m <minutes>\n" Sys.argv.(0);
       exit 1
-  | Some seconds ->
+  | Some (Seconds n | Minutes n) when n <= 0 ->
+      prerr_endline "Timer duration must be greater than 0!\n";
+      exit 1
+  | Some duration ->
+      let seconds =
+        match duration with Seconds n -> n | Minutes n -> n * 60
+      in
+
       for i = seconds downto 1 do
-        Printf.printf "\r%-10d%!" i;
+        Printf.printf "\r%-10s%!" (format_time i);
         Unix.sleepf 1.0
       done;
 
